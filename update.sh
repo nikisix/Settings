9a45df41da4ca43dc84799e0cccae73787b7a34e
ntomasino <nicolai.tesela@gmail.com>
2014-07-01 11:53:52 -0500

    adding bash_aliases to update

--- update.sh
+++ update.sh
@@ -2,6 +2,13 @@ if [ -f ~/.bashrc ]; then
     cp ~/.bashrc . 
 fi
 
+#only for ubuntu 
+#like bashrc, but with just the aliases I define
+#bashrc reads this file in if it exists
+if [ -f ~/.bash_aliases ]; then
+    cp ~/.bash_aliases . 
+fi
+
 if [ -f ~/commands.txt]; then
     cp ~/commands.txt . 
 fi
